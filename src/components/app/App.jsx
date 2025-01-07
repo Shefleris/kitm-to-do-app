0d82bf820f2f65f0c6dca2a1551ea73b0cab868e
Sefleris <p.sefleris@gmail.com>
2025-01-07 14:11:01 +0200

    Remove landing page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../user/Login";
 import Register from "../user/Register";
-import LandingPage from "../landingPage/LandingPage";
 import ProjectList from "../ProjectList/ProjectList";
 import ProjectPage from "../ProjectPage/ProjectPage";
 import AddProject from "../addProject/AddProject";
@@ -15,7 +14,7 @@ function App() {
 			<Router>
 				<Header />
 				<Routes>
-					<Route path="/" element={<LandingPage/>} />
+					<Route path="/" element={<Login/>} />
 					<Route path="/dashboard" element={<ProjectList/>} />
 					<Route path="/login" element={<Login onSuccessRedirectRoute="/dashboard" />} />
 					<Route path="/register" element={<Register onSuccessRedirectRoute="/dashboard" />} />
